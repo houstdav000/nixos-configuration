6cfe693167b6b8f9ab89b5ae5ccd363993fd5a2c
David Houston <houstdav000@gmail.com>
2020-07-12 16:24:06 -0400

    Try to fix sway loading

--- configuration.nix
+++ configuration.nix
@@ -9,6 +9,7 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ./podman-configuration.nix
+      ./sway-configuration.nix
     ];
 
   # Use the systemd-boot EFI boot loader.
