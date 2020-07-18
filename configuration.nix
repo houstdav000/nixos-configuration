75a75f605d5d88eef182ac4c19afcb65aa0fe1b3
David Houston <houstdav000@gmail.com>
2020-07-18 19:34:25 -0400

    Add firefox, thunderbird, alacritty to user pkgs

--- configuration.nix
+++ configuration.nix
@@ -77,23 +77,12 @@
   sound.enable = true;
   hardware.pulseaudio.enable = true;
 
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-  # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = "eurosign:e";
-
-  # Enable touchpad support.
-  # services.xserver.libinput.enable = true;
-
-  # Enable the KDE Desktop Environment.
-  # services.xserver.displayManager.sddm.enable = true;
-  # services.xserver.desktopManager.plasma5.enable = true;
-
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.david = {
     packages = with pkgs; [
       git gnupg
       fish starship exa hexyl bat
+      firefox thunderbird alacritty
     ];
     isNormalUser = true;
     home = "/home/david";
