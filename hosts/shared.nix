c69997227b89bc486ed5468bbd43574090d8c667
David Houston <houstdav000@gmail.com>
2020-07-20 19:09:30 -0400

    Add neovim to hosts/shared.nix

--- hosts/shared.nix
+++ hosts/shared.nix
@@ -25,7 +25,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    wget git gnupg
+    wget git gnupg neovim
     neofetch htop
     nixos-grub2-theme nixos-icons
   ];
