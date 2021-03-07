2710276a17e63559421db365286f83ebbcd1ac17
David Houston <houstdav000@gmail.com>
2021-03-07 00:17:37 -0500

    Add config files to podman service definition

--- services/podman.nix
+++ services/podman.nix
@@ -3,8 +3,41 @@
 # Podman configuration
 
 { config, pkgs, lib, ... }: {
+  environment.systemPackages = with pkgs; [
+    shadow
+  ];
+
   virtualisation.podman = {
     enable = true;
     dockerCompat = true;
+  };
+
+  # Set containerpolicy
+  environment.etc."containers/policy.json" = {
+    mode="0644";
+    text=''
+      {
+        "default": [
+          {
+            "type": "insecureAcceptAnything"
+          }
+        ],
+        "transports":
+          {
+            "docker-daemon":
+              {
+                "": [{"type":"insecureAcceptAnything"}]
+              }
+          }
+      }
+    '';
+  };
+
+  environment.etc."containers/registry.conf" = {
+    mode="0644";
+    text=''
+      [registries.search]
+      registries = ['docker.io', 'quay.io']
+    '';
   };
 }
