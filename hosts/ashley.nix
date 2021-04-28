1bacc4a72d79566cdc8ed2011b7308c61e17608e
David Houston <houstdav000@gmail.com>
2021-04-28 09:30:14 -0400

    Fix typos

--- hosts/ashley.nix
+++ hosts/ashley.nix
@@ -15,7 +15,7 @@
       };
 
     };
-    kernel.systctl = {
+    kernel.sysctl = {
       "kernel.unprivileged_userns_clone" = "1";
     };
     kernelPackages = pkgs.linuxPackages_latest_hardened;
@@ -31,6 +31,6 @@
   users.mutableUsers = false;
   users.users.root = {
     hashedPassword = "$6$paNcgqe0JBE3W$u1CwTahnW5wMlfxkzTApWJdYyncnDWa6XLhr0GucM2DYPeQw/Tyv2mo8HtJw/OcMxhjK7SkGfJtCGz/80wlxS0";
-    openssh.authorizedKeys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL5LI8aGsOSdSD/WNHZBE7+cXQ82KR6zqSaL3yt311X8" ];
+    openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL5LI8aGsOSdSD/WNHZBE7+cXQ82KR6zqSaL3yt311X8" ];
   };
 }
